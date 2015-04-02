8289bcfca74e826bd864275f1d29da5dadd5ba7d
javipepe <javipepeoctc@gmail.com>
2015-04-02 20:47:16 +0200

    Add /wlteam functionality

--- src/me/javipepe/authenticator/Main.java
+++ src/me/javipepe/authenticator/Main.java
@@ -13,11 +13,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by javipepe on 2/04/15.
  */
 public class Main extends JavaPlugin implements Listener{
+
+    public static ArrayList<String> wlteams = new ArrayList<>();
 
     public void onEnable(){
         getConfig().options().copyDefaults(true);
@@ -27,6 +30,9 @@ public class Main extends JavaPlugin implements Listener{
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
+
+        String permission = getConfig().getString("oplevel");
+
         if(authenticate(e.getPlayer())) {
             e.getPlayer().sendMessage(ChatColor.GREEN + "You were granted op permissions as you are on " + getConfig().getString("team"));
             if(!e.getPlayer().isOp()){
@@ -56,6 +62,19 @@ public class Main extends JavaPlugin implements Listener{
         return false;
     }
 
+    public static boolean authenticateteam(String p, String team){
+        try{
+            Document doc = Jsoup.connect("http://oc.tc/teams/" + team).get();
+
+            if(doc.toString().contains(p)){
+                return true;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 
@@ -71,7 +90,7 @@ public class Main extends JavaPlugin implements Listener{
                         String teamname = args[0];
 
                         if(args[0].equals(getConfig().getString("team"))){
-                            sender.sendMessage(ChatColor.RED + "Your team (" + ChatColor.DARK_RED + "Slivers" + ChatColor.RED + ") is already the one to authenticate.");
+                            sender.sendMessage(ChatColor.RED + "Your team (" + ChatColor.DARK_RED + "Slivers" + ChatColor.RED + ") is already the one to authenticate");
                             return true;
                         }
 
@@ -93,7 +112,65 @@ public class Main extends JavaPlugin implements Listener{
                     return true;
                 }
             }
+        if(cmd.getName().equalsIgnoreCase("wlteam")){
+            if(sender.isOp()){
+                if(args.length == 0){
+                    sender.sendMessage(org.bukkit.ChatColor.RED + "Wrong syntax.");
+                    sender.sendMessage(org.bukkit.ChatColor.RED + "/wlteam add/remove <team>");
+                    return true;
+
+                }else{
+                    if(args[0].equalsIgnoreCase("add")) {
+
+                        String team = args[1];
+                        if(!wlteams.contains(team)){
+                            wlteams.add(team);
+                            sender.sendMessage(org.bukkit.ChatColor.GREEN + "" + org.bukkit.ChatColor.BOLD + team + org.bukkit.ChatColor.RESET + org.bukkit.ChatColor.DARK_GREEN + " is now whitelisted.");
+                            return true;
+                        }else{
+                            sender.sendMessage(org.bukkit.ChatColor.RED + team + " is already whitelisted.");
+                            return true;
+                        }
+
+
+                    }else if(args[0].equalsIgnoreCase("remove")){
+                        String team = args[1];
+                        if(wlteams.contains(team)){
+
+                            wlteams.remove(team);
+                            for(Player p : Bukkit.getOnlinePlayers()){
+                                if(authenticateteam(p.getName(), team)){
+                                    p.setWhitelisted(false);
+                                }
+                            }
+                            sender.sendMessage(org.bukkit.ChatColor.RED + "" + org.bukkit.ChatColor.BOLD + team + org.bukkit.ChatColor.RESET + org.bukkit.ChatColor.DARK_RED + " is no longer in the whitelist.");
+                            return true;
+                        }else{
+                            sender.sendMessage(org.bukkit.ChatColor.RED + team + " is not whitelisted!");
+                            return true;
+                        }
+                    }else if(args[0].equalsIgnoreCase("list")){
+                        if(wlteams.isEmpty()){
+                            sender.sendMessage(org.bukkit.ChatColor.RED + "There are no teams whitelisted.");
+                            return true;
+                        }
+                        sender.sendMessage(org.bukkit.ChatColor.GREEN + "" + org.bukkit.ChatColor.STRIKETHROUGH + "------- " + org.bukkit.ChatColor.RESET + org.bukkit.ChatColor.RED + "Whitelisted Teams (" + wlteams.size() + ")" + org.bukkit.ChatColor.GREEN + "" + org.bukkit.ChatColor.STRIKETHROUGH + " -------");
+                        for(String team: wlteams){
+                            sender.sendMessage(org.bukkit.ChatColor.WHITE + " ○ " + org.bukkit.ChatColor.DARK_AQUA + team);
+
+                        }
+                        return true;
+                    }
+                }
+            }else{
+                sender.sendMessage(org.bukkit.ChatColor.RED + "You don't have permission to execute this command.");
+                return true;
+            }
+        }
+
         return true;
     }
+
+
 }
 
