acf876dfc2b023ffdef676fb69528a2070cf9a52
javipepe <javipepeoctc@gmail.com>
2015-04-03 18:47:11 +0200

    Add blacklist listener to the prelogin listener

--- src/me/javipepe/authenticator/listeners/AsyncPreLoginListener.java
+++ src/me/javipepe/authenticator/listeners/AsyncPreLoginListener.java
@@ -1,6 +1,7 @@
 package me.javipepe.authenticator.listeners;
 
 import me.javipepe.authenticator.Main;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,12 +14,21 @@ public class AsyncPreLoginListener implements Listener {
     @EventHandler
     public void onPreLogin(AsyncPlayerPreLoginEvent e) {
         String playername = e.getName();
-
+        //whitelist
         for (String team : Main.wlteams) {
             if (Main.authenticateteam(playername, team)) {
                 Bukkit.getPlayer(playername).setWhitelisted(true);
                 //Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "minecraft:whitelist add " + playername);
                 e.allow();
+
+            }
+        }
+
+        //blacklist
+        for (String team : Main.blteams) {
+            if (Main.authenticateteam(playername, team)) {
+                e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, ChatColor.GRAY + "" + ChatColor.BOLD + "Your team (" + team + ") is blacklisted from the server.");
+
 
             }
         }
